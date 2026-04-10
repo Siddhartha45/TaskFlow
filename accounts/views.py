6d42e1ec460b1ab2e882b7a48c86a9bdf75fb29f
Siddhartha45 <87509046+Siddhartha45@users.noreply.github.com>
2026-04-10 20:00:56 +0545

    auth added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
