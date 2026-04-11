3c6ca4955a2327b5aacbc427bdb5a26b69286662
Siddhartha45 <87509046+Siddhartha45@users.noreply.github.com>
2026-04-11 14:37:25 +0545

    completed project

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ProjectListCreateView, ProjectDetailView
+
+
+urlpatterns = [
+    path('projects/', ProjectListCreateView.as_view()),
+    path('projects/<int:pk>/', ProjectDetailView.as_view()),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ProjectSerializer
+from .models import Project
 
-# Create your views here.
+
+class ProjectListCreateView(ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
+    def get_queryset(self):
+        user = self.request.user
+        return user.projects.all()
+
+
+class ProjectDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return user.projects.all()
+        
