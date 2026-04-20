e0619f82fc5cd0a2f8ef302228129353e93a93ec
Siddhartha45 <87509046+Siddhartha45@users.noreply.github.com>
2026-04-21 00:15:05 +0545

    added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Use this version of python image, slim strips unnecessary system packages
+FROM python:3.10-slim
+
+# Set the app dir as working directory, also create app dir if it doesn't exist 
+WORKDIR /app
+
+# Stops python from writing .pyc files, __pycache__ folders
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Forces python to flush output immediately, print statement and logs show up in real time, imp for debugging
+ENV PYTHONUNBUFFERED=1
+
+RUN pip install --upgrade pip
+
+# Copy requirements.txt file from our project folder to current working dir
+COPY requirements.txt .
+
+# Install dependencies without storing local cache
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy files and folders of our project to current working dir
+COPY . .
+
+# Signals this container expects to recieve traffic on 8000
+EXPOSE 8000
+
+# Executes when the container starts
+CMD ["gunicorn", "taskflow.wsgi:application", "--bind", "0.0.0.0:8000"]
--- taskflow/settings.py
+++ taskflow/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from celery.schedules import crontab
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(pn9+iacyt_f=a&*kbq%ny!pqv=hrs$+#t)46pq@90#%m6=8qf"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,26 +140,23 @@ AUTH_USER_MODEL = "accounts.User"
 
 # For development(increasing token liftime)
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'your_email@example.com'
-# EMAIL_HOST_PASSWORD = 'your_password'
-DEFAULT_FROM_EMAIL = 'your_email@example.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_TIMEZONE = TIME_ZONE
-
+CELERY_BROKER_URL = config('CELERY_BROKER_URL')
 
 # Runs task_reminder function daily at 9:00 AM UTC
 CELERY_BEAT_SCHEDULE = {
